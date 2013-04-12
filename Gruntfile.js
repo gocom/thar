b742bd688aebd99083528769bd5ca95eab065a8e
Jukka Svahn <web@rahinaa.biz>
2013-04-13 00:25:23 +0300

    Remove extra space.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt)
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.initConfig({
-		pkg  : grunt.file.readJSON('package.json'),
+		pkg : grunt.file.readJSON('package.json'),
 		uglify :
 		{
 			options :
