766fc4066a853dcc4bd62ffd4c628fb9c9eacde4
Jukka Svahn <web@rahinaa.biz>
2013-04-13 20:52:42 +0300

    Added grunt-bumpup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function (grunt)
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-conventional-changelog');
+	grunt.loadNpmTasks('grunt-bumpup');
 
 	grunt.initConfig({
 		pkg : grunt.file.readJSON('package.json'),
@@ -46,9 +48,30 @@ module.exports = function (grunt)
 					jQuery : true
 				}
 			}
+		},
+
+		changelog :
+		{
+			options :
+			{
+				dest : 'CHANGELOG.md'
+			}
+		},
+
+		bumpup :
+		{
+			files : ['package.json']
 		}
 	});
 
 	grunt.registerTask('default', ['jshint', 'uglify', 'copy']);
+	grunt.registerTask('changelog', ['changelog']);
 	grunt.registerTask('debug', ['jshint']);
+
+	grunt.registerTask('release', function (type)
+	{
+		type = type ? type : 'patch';
+		grunt.task.run('default');
+		grunt.task.run('bumpup:' + type);
+	});
 };
